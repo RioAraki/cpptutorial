9ac794f5b47ea5d668e7f510b2606561b1acb10e
RioAraki <yueee.li@mail.utoronto.ca>
2018-02-01 00:38:41 -0500

    finished how to write a c++ class

--- tut1/Project1/Project1/main.cpp
+++ tut1/Project1/Project1/main.cpp
@@ -1,14 +1,46 @@
 # include <iostream>
-# include "Log.h"
+
+
+class Log {
+
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo; //class member variable which is private 
+
+public:
+	void SetLevel(int level) {
+		m_LogLevel = level;
+	}
+
+	void Warn(const char* message) {
+		if (m_LogLevel >= LogLevelWarning) {
+			std::cout << "[WARNING]: " << message << std::endl;
+		}
+	}
+
+	void Error(const char* message) {
+		if (m_LogLevel >= LogLevelError) {
+			std::cout << "[ERROR]: " << message << std::endl;
+		}
+	}
+
+	void Info(const char* message) {
+		if (m_LogLevel >= LogLevelInfo) {
+			std::cout << "[INFO]: " << message << std::endl;
+		}
+	}
+
+};
 
 int main() {
 
-	int x = 5;
-	bool comparisonResult = x == 5;
-	if (comparisonResult) {
-		
-	}
-
-	Log("Hello World!");
+	Log log; // instantiate the object
+	log.SetLevel(log.LogLevelInfo);
+	log.Warn("Warning!");
+	log.Info("Warning!");
+	log.Error("Warning!");
 	std::cin.get();
 }
